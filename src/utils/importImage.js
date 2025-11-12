cd0b732ddfa4b8e34e7c8d462bdf970e4700b43e
Naomi-Aiy <111712363+Naomi-Aiy@users.noreply.github.com>
2025-11-12 11:56:42 -0500

    Update site content

--- src/utils/importImage.js
+++
@@ -1,9 +0,0 @@
-const importAll = (r) => {
-    let images = {};
-    r.keys().map((item, index) => { images[item.replace('./', '')] = r(item); });
-    return images;
-  }
-  
-  const images = importAll(require.context('../assets/slides', false, /\.(png|jpe?g|svg)$/));
-  
-  export default images;
